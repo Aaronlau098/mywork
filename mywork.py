dfb7e4f2fba5b3be6e85db5b4da03da5cd268082
Aaronlau098 <390184725@qq.com>
2022-01-07 14:47:14 +0800

    first commit

---
+++ mywork.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+
+if __name__ == "__main__":
+    data = pd.read_excel("mytest.xlsx")
+    print(data.head())
+    print("hello world!!")
+    print("hell world!!")
+    print("hell world!!")
+    print("hell world!!")
