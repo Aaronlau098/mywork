4e51af6f60c9be516a7e2615265590160c99af98
Aaronlau098 <390184725@qq.com>
2022-01-07 14:48:27 +0800

    my second commit

--- mywork.py
+++ mywork.py
@@ -9,3 +9,7 @@ if __name__ == "__main__":
     print("hell world!!")
     print("hell world!!")
     print("hell world!!")
+    print("hell world!!")
+    print("hell world!!")
+    print("hell world!!")
+    print("hell world!!")
